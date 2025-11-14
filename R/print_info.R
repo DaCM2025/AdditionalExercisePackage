be2ca1aa2b71b2f5035c81c683d2b97a92264019
Timofei Shashkov <timofei98shashkov@gmail.com>
2025-11-14 14:50:48 +0100

    Fixed first agrument mismatch between the generic print_info function and its method description

--- R/print_info.R
+++ R/print_info.R
@@ -1,6 +1,6 @@
 #' Print method for a class Person_class
 #'
-#' @param x an object to which we apply our method
+#' @param object an object to which we apply our method
 #' @param ... Additional parameters (ignored)
 #'
 #' @return Prints information about the person
@@ -9,8 +9,8 @@
 #'
 #' @export
 #' @method print_info Person_class
-print_info.Person_class <- function(x, ...) {
+print_info.Person_class <- function(object, ...) {
   cat("Person:\n")
-  cat("  Name:", x$name, "\n")
-  cat("  Age :", x$age, "\n")
+  cat("  Name:", object$name, "\n")
+  cat("  Age :", object$age, "\n")
 }
